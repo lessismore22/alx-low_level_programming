ab6cd4ccc0300301d5410a8bb3f76ddb293527f4
Charles <Charlesigbe0@gmail.com>
2023-10-07 19:29:50 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - this prints the letters of the alphabet from A-Z
+ * in lower case
+ *
+ *
+*/
+int main(void)
+{
+char letters;
+	for (letters = a; letter <= z; letter++)
+	{
+	putchar(letter);
+	putchar('\n');
+	}
+}
