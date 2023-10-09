efc6e94b3dad285eede8e7bf434c2fe1b9f7a045
Charles <Charlesigbe0@gmail.com>
2023-10-09 16:40:51 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main- this function prints a combination of all
+ * single digit numbers with commas
+ *
+ * Return:0 always success;
+ */
+
+int main(void)
+#include <stdio.h>
+{
+	int n;
+
+	for (n = '0'; c <= '9'; n++)
+		putchar(c);
+	{
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
