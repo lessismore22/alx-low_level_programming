456f11bae4c8784564b6254fd7742924feb88462
Charles <Charlesigbe0@gmail.com>
2023-10-10 12:55:26 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main- this functions prints _putchar into a
+ * new line
+ *
+ * Return:0 always successful
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i = 0;
+
+	while (i = 0; != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
