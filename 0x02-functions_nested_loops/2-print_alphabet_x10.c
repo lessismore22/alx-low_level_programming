34a572d397dfd7a17c12e28969841a57951e0261
Charles <Charlesigbe0@gmail.com>
2023-10-10 20:11:01 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet for 10 counts
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count;
+
+	for (count = '1'; count <= '10'; count++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
