0db078bd78efdf9b4a826a89588905cab6433d5e
Charles <Charlesigbe0@gmail.com>
2023-10-11 10:40:17 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *isalpha - this function prints all
+ * alphabets in lower or uppercase
+ * @c: character type
+ * Return:1 if alphabet and 0 if otherwise
+ */
+int _isalpha(int c);
+{
+	if (c >= 'a' || c >= 'A' && c <= 'z' || c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
