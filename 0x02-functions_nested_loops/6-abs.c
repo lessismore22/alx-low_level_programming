d6b426a9fd4fcb937abac1d5e562f9feaf2d58a3
Charles <Charlesigbe0@gmail.com>
2023-10-11 13:42:53 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _abs - this function prints the absolute value of a digit
+ * @int: character type
+ * Return: 0 if succesful
+ *
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n > 0)
+	{
+		_putchar(n);
+	}
+
+	else if (n < 0)
+	{
+		_putchar(n);
+	}
+
+	else
+	{
+		_putchar(0);
+	}
+
+	return (0);
+}
