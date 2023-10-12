47e4989f785a2583cf7eb8edb41da59bec2c6466
Charles <Charlesigbe0@gmail.com>
2023-10-11 20:11:09 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - this function prints the complete time in a day
+ *
+ * Return: 0
+ *
+ */
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar((':)
+			_putchar((y/10) + '0');
+			_putchar((y%10) + '0');
+			_putchar(''\n');
+
+		}
+	}
+}
