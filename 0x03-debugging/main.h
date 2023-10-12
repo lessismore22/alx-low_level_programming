1466e851c0b6a92df5c52a0bac67f9ceb7e5a970
Charles <Charlesigbe0@gmail.com>
2023-10-12 13:36:09 -0700

    0-main.c, main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * positive_or_negative - tests function if it is positive
+ * or negative.
+ */
+
+void positive_or_negative(int i)
+
+#endif /*MAIN_H */
+
+
