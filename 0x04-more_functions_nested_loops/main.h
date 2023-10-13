52c9426a313a3065ad8b0a5324fe9a714253f473
Charles <Charlesigbe0@gmail.com>
2023-10-13 16:57:20 -0700

    general fix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,11 @@
-int _isupper(int c);
+#ifndef MAIN_Hint _isupper
+#define MAIN_H
+
+(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void print_most_numbers(void);
+
+#endif
