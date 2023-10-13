e6f85cb0eca1980997044dc6a86515c0c897c720
Charles <Charlesigbe0@gmail.com>
2023-10-13 10:20:47 -0700

    prinnt nummbrs

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,11 @@
 /**
  * print_numbers - this function prints the numbers from
  * 0 to 9, followed by a new line
- * 
  * Return: 0 if succesful
  */
 void print_numbers(void)
 {
-	int num;
+	char num;
 	
 	for (num = '0'; num <= '9'; num++)
 	{
