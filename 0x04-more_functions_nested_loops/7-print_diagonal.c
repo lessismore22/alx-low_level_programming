ff94d7fb2c3412d221fdb662296c3ca1322550d8
Charles <Charlesigbe0@gmail.com>
2023-10-15 17:09:56 -0700

    print line fixx

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,18 +10,22 @@
 void print_diagonal(int n)
 {
 	int i;
-	
+	int j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(92);
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
 		}
 	}
-
+	_putchar('\\');
 	_putchar('\n');
+	}
 }
