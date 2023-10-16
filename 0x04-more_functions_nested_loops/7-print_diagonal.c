c2ad749c6bfbd7c7d5aad39cbcf46b0d65f58bad
Charles <Charlesigbe0@gmail.com>
2023-10-16 00:37:22 -0700

    print diagonal line final fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -20,12 +20,13 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-		for (j = 0; j < i; j++)
-		{
-			_putchar(' ');
+			for (j = 0; j < i; j++)
+			{
+				_putchar(32);
+			}
+
+			_putchar(92);
+			_putchar('\n');
 		}
-	}
-	_putchar('\\');
-	_putchar('\n');
 	}
 }
