3a6d3b303176174c07d9f518b6daa5c0e8668275
Charles <charlesigbe0@gmail.com>
2023-10-20 02:52:47 +0000

    strncat fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -23,11 +23,9 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
 	{
-		dest[i + j] = src[j];
+		dest[i] = src[j];
 		i++;
 	}
-
-	dest[i + j] = '\0';
 
 	return (dest);
 
