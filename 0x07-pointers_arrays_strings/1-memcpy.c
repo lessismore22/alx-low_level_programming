53537d3fca96ba7bbbc3c346a7b8a4b189d79266
Charles <charlesigbe0@gmail.com>
2023-10-23 16:14:49 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * simple_print_buffer - this function copies a memory area
+ * @dest: where the byte will be copied to
+ * @src: where the byte will be copied from
+ * n: number of bytes from the memory area.
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	int tmp;
+
+	for (i = 0; i < n; i++)
+	{
+		src[i] = tmp;
+		tmp = dest[i];
+
+	}
+	
+	return (dest);
+}	
