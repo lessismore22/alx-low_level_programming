989c37c167b7114ae136920bcc63f332cd6f7919
Charles <charlesigbe0@gmail.com>
2023-11-07 15:49:12 +0000

    2.main.c fix fixx

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - this program prints the file it was compiled from
+ * Return: 0 if succesful
+ */
+int main(void)
 {
-	printf("%s\n", __FILE__);	
-	
+	printf("%s\n", __FILE__);
+
 	return (0);
 }
