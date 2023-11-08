2bdd27caf8bbf64d38867daa37dcf23cb1ead0ad
Charles <charlesigbe0@gmail.com>
2023-11-08 01:39:40 +0000

    dog.h fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
-struct User
+#ifndef DOG_H
+#define DOG_H
+
+ /**
+ * struct dog - this is a new type "struct dog" with the basic info of a dog
+ * @name:char
+ * @age: float
+ * @owner: char to check
+ * be right back
+ */struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-}
+};
+
