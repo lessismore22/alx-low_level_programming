8f82b66ab12a2f2cec9a07f817aab19e85e78cce
Charles <charlesigbe0@gmail.com>
2023-11-08 04:45:42 +0000

    dogg fixx

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
- /**
+/**
 * struct dog - this is a type displaying
 * basic features of a dog
 * @name:char
@@ -21,5 +21,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 dog_t *my_dog;
+
 #endif
 
