ce401b3317a12b87119f8276c0435ae385884a1c
Charles <charlesigbe0@gmail.com>
2023-11-10 04:42:12 +0000

    print numbers fixx

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ag, int));
-	
 	if (separator != NULL && i != n - 1)
 		printf("%s", separator);
 	}
