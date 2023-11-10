cf2fe33e0f1a7e2feb5bdfbb9438f946642a14e0
Charles <charlesigbe0@gmail.com>
2023-11-10 04:39:34 +0000

    variadic fixx

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,6 +2,7 @@
 #define variadic_functions_h
 
 #include <stdarg.h>
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
