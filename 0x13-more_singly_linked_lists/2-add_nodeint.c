27ed18515516aa04f35c5711c4257473fbf7c8a9
Charles <charlesigbe0@gmail.com>
2023-11-21 02:16:41 +0000

    add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - this function adds a new node at the beginning
+ * of a list
+ * @head: head of the list
+ * @n: integer value
+ * Return: pointer to the new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t nodeX;
+
+	node_X = malloc(sizeof(listint_t));
+
+	if (node_X == NULL)
+	{
+		return (NULL);
+	}
+
+	node_X->n = n;
+	node_X->next = *head;
+
+	*head = node_X;
+
+	return (*head);
+}
+
---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - adds a new node at the end of a list
+ * @head: pointer to head of the list
+ * @n: integer n
+ * Return: the pointer to the address
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t node_X;
+
+	node_X = malloc(sizeof(listint_t));
+
+	if (node_X == NULL)
+	{
+		return (NULL);
+
+
+		node_X->n = n;
+
+		node_X->new = NULL;
+
+		tmp = *head;
+
+	}
+
+	if (*head == NULL)
+	{
+		*head = node_X;
+	}
+
+	else
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+
+		tmp->next = node_X;
+	}
+
+	return (node_X);
+}
+
